0a0d641233ae482fe7edba69e356998b8a6a0004
Ebenezer  Semere <ebenezersemere@Ebenezers-MacBook-Air.local>
2023-01-20 22:11:55 -0800

    Init commit

---
+++ M1-Challenge-Semere-Ebenezer/accounting-challenge/src/test/java/com/company/CustomerTest.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Test
+    public void shouldCalculateBalance(){
+
+    }
+
+    @Test
+    public void shouldPrintCustomerDetails(){
+
+    }
+}
